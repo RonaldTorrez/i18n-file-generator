a0128d7ce5c85e1fad6cb42c651ed445731d878a
Ronald Torrez <dev@ronaldtorrez.com>
2024-03-09 16:04:51 -0400

    Updated name of Config Cli

--- src/config/app.config.mjs
+++ src/config/app.config.mjs
@@ -2,4 +2,4 @@ import { loadFromRoot } from '../utility/index.mjs'
 
 const configCliFileName = 'i18n.generator.config.mjs'
 
-export const getConfigCli = import(loadFromRoot(configCliFileName))
+export const readConfigCli = import(loadFromRoot(configCliFileName))
