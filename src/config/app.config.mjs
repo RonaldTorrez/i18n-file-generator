caf7cbee618c5f186e7a49134eb77e72267183cb
Ronald Torrez <dev@ronaldtorrez.com>
2024-03-10 15:43:25 -0400

    Added new functions

--- src/config/app.config.mjs
+++ src/config/app.config.mjs
@@ -3,3 +3,6 @@ import { pathFrom } from '../utility/index.mjs'
 const configCliFileName = 'i18n.generator.config.mjs'
 
 export const readConfigCli = import(pathFrom(true, configCliFileName))
+export const appConfig = {
+	printMessagesForDev: true
+}
--- src/utility/dir.util.mjs
+++ src/utility/dir.util.mjs
@@ -1,11 +1,28 @@
 import { promises as fs } from 'fs'
 import * as path from 'path'
+import { appConfig } from '../config/index.mjs'
 
 export const pathFrom = (fromRoot = false, ...dir) => path.join(fromRoot ? process.cwd() : '', dir.join('/'))
 
-export const readDir = async (path) => await fs.readdir(path).then(data => data)
+export const readDir = async (path, returnFullPath = false) => {
+	const {printMessagesForDev} = appConfig
+
+	try {
+		const files = await fs.readdir(path).then(data => data)
+		return !returnFullPath ? files : files.map(file => pathFrom(false, path, file))
+	} catch (error) {
+		if (printMessagesForDev) {
+			console.error('🚨 error readDir()', path, error)
+		}
+		return []
+	}
+}
 export const readFile = async (path) => await fs.readFile(path, 'utf8').then(data => data)
 export const writeFile = async (path, data) => await fs.writeFile(path, data, 'utf8')
+export const getFilesFromPath = async (path) => {
+	const files = await readDir(path)
+	return files.map(file => pathFrom(true, path, file))
+}
 
 export const isFile = async (path) => await fs.stat(path).then(file => file.isFile())
 export const isDirectory = async (path) => await fs.stat(path).then(file => file.isDirectory())
