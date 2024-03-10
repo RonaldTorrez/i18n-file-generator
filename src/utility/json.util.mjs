be43280bdf2af53026a78af771b0f8b136f8b1a1
Ronald Torrez <dev@ronaldtorrez.com>
2024-03-10 15:44:21 -0400

    Updated and added functions for JSON

--- src/utility/json.util.mjs
+++ src/utility/json.util.mjs
@@ -1,32 +1,62 @@
-import { extName, isDirectory, isFile, pathFrom, readDir, readFile } from './index.mjs'
+import { extName, isDirectory, isFile, readDir, readFile } from './index.mjs'
 
 export const jsonParse = (data) => JSON.parse(data)
 export const jsonStringify = (data) => JSON.stringify(data, null, 2)
 
-export const mergeJson = async (path, subFolder = false) => {
-	const readdir = await readDir(path)
+export const mergeDataFromPath = async (path, fromSubFolder = false) => {
+	const files = await readDir(path, true)
 
-	if (!readdir.length) {
+	if (files && !files.length) {
 		return
 	}
 
 	const data = []
 
-	for (const file of readdir) {
-		const filePath = pathFrom(false, path, file)
-
-		if (subFolder && await isDirectory(filePath)) {
-			const subData = await mergeJson(filePath, subFolder)
+	for (const file of files) {
+		if (fromSubFolder && await isDirectory(file)) {
+			const subData = await mergeDataFromPath(file, fromSubFolder)
 			if (subData && subData.length > 0) {
 				data.push(...subData)
 			}
 		}
 
-		if (await isFile(filePath) && extName(filePath) === '.json') {
-			const content = await readFile(filePath)
+		if (await isFile(file) && extName(file) === '.json') {
+			const content = await readFile(file)
 			data.push(jsonParse(content))
 		}
 	}
 
 	return data
+}
+export const mergeObjs = (array) => {
+	const mergedData = {}
+
+	array.forEach(obj => {
+		for (const key in obj) {
+			if (mergedData.hasOwnProperty(key)) {
+				mergedData[key] = mergeValues(mergedData[key], obj[key])
+			} else {
+				mergedData[key] = obj[key]
+			}
+		}
+	})
+
+	return mergedData
+}
+export const mergeValues = (actualObj, newObj) => {
+	const mergedObj = {...actualObj}
+
+	for (const key in newObj) {
+		if (mergedObj.hasOwnProperty(key)) {
+			if (typeof mergedObj[key] === 'object' && typeof newObj[key] === 'object') {
+				mergedObj[key] = mergeValues(mergedObj[key], newObj[key])
+			} else {
+				mergedObj[key] = newObj[key]
+			}
+		} else {
+			mergedObj[key] = newObj[key]
+		}
+	}
+
+	return mergedObj
 }
