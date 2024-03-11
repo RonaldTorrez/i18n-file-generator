7f80cfb486ce3d65f25d462fa0bc661620ac1fb0
Ronald Torrez <dev@ronaldtorrez.com>
2024-03-10 23:42:23 -0400

    Optimized functions of dir.util.mjs

--- src/utility/dir.util.mjs
+++ src/utility/dir.util.mjs
@@ -6,26 +6,66 @@ import { consoleError } from './general.util.mjs'
 export const pathFrom = (fromRoot = false, ...dir) => path.join(fromRoot ? process.cwd() : '', dir.join('/'))
 
 export const readDir = async (path, returnFullPath = false) => {
-	const {printMessagesForDev} = appConfig
-
 	try {
 		const files = await fs.readdir(path).then(data => data)
 		return !returnFullPath ? files : files.map(file => pathFrom(false, path, file))
 	} catch (error) {
-		if (printMessagesForDev) {
+		if (appConfig.printMessagesForDev) {
 			consoleError('readDir()', path, error)
 		}
 	}
 }
-export const readFile = async (path) => await fs.readFile(path, 'utf8').then(data => data)
-export const writeFile = async (path, data) => await fs.writeFile(path, data, 'utf8')
-export const getFilesFromPath = async (path) => {
-	const files = await readDir(path)
-	return files.map(file => pathFrom(true, path, file))
+
+export const readFile = async (path) => {
+	try {
+		return await fs.readFile(path, 'utf8').then(data => data)
+	} catch (error) {
+		if (appConfig.printMessagesForDev) {
+			consoleError('readFile()', path, error)
+		}
+	}
 }
 
-export const isFile = async (path) => await fs.stat(path).then(file => file.isFile())
-export const isDirectory = async (path) => await fs.stat(path).then(file => file.isDirectory())
-export const existPath = async (path) => await fs.access(path)
+export const writeFile = async (path, data) => {
+	try {
+		await fs.writeFile(path, data, 'utf8')
+	} catch (error) {
+		if (appConfig.printMessagesForDev) {
+			consoleError('writeFile()', path, error)
+		}
+	}
+}
+
+export const isFile = async (path) => {
+	try {
+		return await fs.stat(path).then(file => file.isFile())
+	} catch (error) {
+		if (appConfig.printMessagesForDev) {
+			consoleError('isFile()', path, error)
+		}
+	}
+}
+
+export const isDirectory = async (path) => {
+	try {
+		return await fs.stat(path).then(file => file.isDirectory())
+	} catch (error) {
+		if (appConfig.printMessagesForDev) {
+			consoleError('isDirectory()', path, error)
+		}
+	}
+}
+
+export const existPath = async (path) => {
+	try {
+		await fs.access(path)
+		return true
+	} catch (error) {
+		if (appConfig.printMessagesForDev) {
+			consoleError('existPath()', path, error)
+		}
+		return false
+	}
+}
 
 export const extName = (file) => path.extname(file)
