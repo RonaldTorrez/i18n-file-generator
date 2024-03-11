05bb7337b8c68747c382e06ad202c62ae7c59834
Ronald Torrez <dev@ronaldtorrez.com>
2024-03-10 23:43:56 -0400

    Updated mergeDataFromPath and added getDeepKeys functions

--- src/utility/json.util.mjs
+++ src/utility/json.util.mjs
@@ -1,33 +1,43 @@
-import { extName, isDirectory, isFile, readDir, readFile } from './index.mjs'
+import { appConfig } from '../config/index.mjs'
+import { consoleError, extName, isDirectory, isFile, isNull, readDir, readFile } from './index.mjs'
 
 export const jsonParse = (data) => JSON.parse(data)
 export const jsonStringify = (data) => JSON.stringify(data, null, 2)
 
 export const mergeDataFromPath = async (path, fromSubFolder = false) => {
-	const files = await readDir(path, true)
+	const mergedData = []
 
-	if (files && !files.length) {
-		return
-	}
+	const mergeDataRecursively = async (path, fromSubFolder = false) => {
+		try {
+			const files = await readDir(path, true)
 
-	const data = []
+			if (!files || isNull(files)) {
+				return
+			}
 
-	for (const file of files) {
-		if (fromSubFolder && await isDirectory(file)) {
-			const subData = await mergeDataFromPath(file, fromSubFolder)
-			if (subData && subData.length > 0) {
-				data.push(...subData)
+			for (const file of files) {
+				if (fromSubFolder && await isDirectory(file)) {
+					await mergeDataRecursively(file, fromSubFolder)
+				} else if ((
+					           await isFile(file)
+				           ) && extName(file) === '.json'
+				) {
+					const content = await readFile(file)
+					mergedData.push(jsonParse(content))
+				}
+			}
+		} catch (error) {
+			if (appConfig.printMessagesForDev) {
+				consoleError('mergeDataRecursively()', path, error)
 			}
 		}
-
-		if (await isFile(file) && extName(file) === '.json') {
-			const content = await readFile(file)
-			data.push(jsonParse(content))
-		}
 	}
 
-	return data
+	await mergeDataRecursively(path, fromSubFolder)
+
+	return mergedData
 }
+
 export const mergeObjs = (array) => {
 	const mergedData = {}
 
@@ -59,4 +69,22 @@ export const mergeValues = (actualObj, newObj) => {
 	}
 
 	return mergedObj
+}
+
+export const getDeepKeys = (obj) => {
+	const languages = []
+
+	const getKey = (obj) => {
+		for (const key in obj) {
+			if (typeof obj[key] === 'object' && obj[key] !== null) {
+				getKey(obj[key])
+			} else if (!languages.includes(key)) {
+				languages.push(key)
+			}
+		}
+	}
+
+	getKey(obj)
+
+	return languages
 }
